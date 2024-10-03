dba230cac0ccad5cd40521ca54b62e961aeb0cdc
Sharon Zhou <szho0067@student.monash.edu>
2024-10-03 23:17:23 +1000

    Male Proportions By Year and Embed 2 Idioms

--- vega_lite_vs.js
+++ vega_lite_vs.js
@@ -1,3 +1,7 @@
 var vg_1 = "map_5.vg.json";
-vegaEmbed("#map", vg_1).then(function(result) {
+vegaEmbed("#map1", vg_1).then(function(result) {
+}).catch(console.error);
+
+var vg_2 = "industry_growth.vg.json";
+vegaEmbed("#map2", vg_2).then(function(result) {
 }).catch(console.error);
