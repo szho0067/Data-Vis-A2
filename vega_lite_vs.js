82824927a15269ecc2ed799487c4b6e330f2a7c7
Sharon Zhou <szho0067@student.monash.edu>
2024-10-08 10:49:59 +1100

    index

--- vega_lite_vs.js
+++ vega_lite_vs.js
@@ -5,3 +5,11 @@ vegaEmbed("#map1", vg_1).then(function(result) {
 var vg_2 = "industry_growth.vg.json";
 vegaEmbed("#map2", vg_2).then(function(result) {
 }).catch(console.error);
+
+var vg_3 = "Gender_employment_growth.vg.json";
+vegaEmbed("#map3", vg_3).then(function(result) {
+}).catch(console.error);
+
+var vg_4 = "test_dp_v2.vg.json";
+vegaEmbed("#map4", vg_4).then(function(result) {
+}).catch(console.error);
