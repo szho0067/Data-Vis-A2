ed491f8ba1daa7bf293c136e89333bd534814839
Sharon Zhou <szho0067@student.monash.edu>
2024-10-08 13:02:55 +1100

    Add colour and structure to dashboard

--- vega_lite_vs.js
+++ vega_lite_vs.js
@@ -10,6 +10,6 @@ var vg_3 = "Gender_employment_growth.vg.json";
 vegaEmbed("#map3", vg_3).then(function(result) {
 }).catch(console.error);
 
-var vg_4 = "test_dp_v2.vg.json";
+var vg_4 = "test_dp_v3.vg.json";
 vegaEmbed("#map4", vg_4).then(function(result) {
 }).catch(console.error);
