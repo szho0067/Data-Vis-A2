83ba272cdf41f9400d69861002fa1918e07d7f3f
Sharon Zhou <szho0067@student.monash.edu>
2024-10-09 10:28:21 +1100

    Males anad Females Proportion Data

--- vega_lite_vs.js
+++ vega_lite_vs.js
@@ -2,11 +2,11 @@ var vg_1 = "map_5.vg.json";
 vegaEmbed("#map1", vg_1).then(function(result) {
 }).catch(console.error);
 
-var vg_2 = "industry_growth.vg.json";
+var vg_2 = "industry_growth_v2.vg.json";
 vegaEmbed("#map2", vg_2).then(function(result) {
 }).catch(console.error);
 
-var vg_3 = "Gender_employment_growth.vg.json";
+var vg_3 = "Gender_employment_growth_v2.vg.json";
 vegaEmbed("#map3", vg_3).then(function(result) {
 }).catch(console.error);
 
