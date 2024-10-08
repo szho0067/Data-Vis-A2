206d745de952914f690cc5472eccd486ae0da1dc
Sharon Zhou <szho0067@student.monash.edu>
2024-10-08 15:14:50 +1100

    Update html

--- vega_lite_vs.js
+++ vega_lite_vs.js
@@ -13,3 +13,15 @@ vegaEmbed("#map3", vg_3).then(function(result) {
 var vg_4 = "test_dp_v3.vg.json";
 vegaEmbed("#map4", vg_4).then(function(result) {
 }).catch(console.error);
+
+var vg_5 = "Female_Proportions_By_Year.vg.json";
+vegaEmbed("#map5", vg_5).then(function(result) {
+}).catch(console.error);
+
+var vg_6 = "Male_Proportions_By_Year.vg.json";
+vegaEmbed("#map6", vg_6).then(function(result) {
+}).catch(console.error);
+
+var vg_7 = "Gender_pay_gap_test.vg.json";
+vegaEmbed("#map7", vg_7).then(function(result) {
+}).catch(console.error);
