b93599873f2c128661f6f0e7757490c2d780d11e
Sharon Zhou <szho0067@student.monash.edu>
2024-10-13 20:47:37 +1100

    Finalised Assignment

--- vega_lite_vs.js
+++ vega_lite_vs.js
@@ -3,38 +3,30 @@ vegaEmbed("#map1", vg_1, {actions: false}).then(function(result) {
 }).catch(console.error);
 
 var vg_2 = "industry_growth_v2.vg.json";
-vegaEmbed("#map2", vg_2, {actions: false}).then(function(result) {
+vegaEmbed("#industry_growth", vg_2, {actions: false}).then(function(result) {
 }).catch(console.error);
 
 var vg_3 = "Gender_employment_growth_v2.vg.json";
-vegaEmbed("#map3", vg_3, {actions: false}).then(function(result) {
+vegaEmbed("#gender_growth", vg_3, {actions: false}).then(function(result) {
 }).catch(console.error);
 
 var vg_4 = "test_dp_v3.vg.json";
-vegaEmbed("#map4", vg_4, {actions: false}).then(function(result) {
+vegaEmbed("#dotplot", vg_4, {actions: false}).then(function(result) {
 }).catch(console.error);
 
 var vg_5 = "Female_100_chart.vg.json";
 vegaEmbed("#map5", vg_5, {actions: false}).then(function(result) {
 }).catch(console.error);
 
-var vg_6 = "Male_Proportions_By_Year.vg.json";
-vegaEmbed("#map6", vg_6, {actions: false}).then(function(result) {
-}).catch(console.error);
-
-var vg_7 = "gender_pay_gap.vg.json";
-vegaEmbed("#map7", vg_7, {actions: false}).then(function(result) {
-}).catch(console.error);
-
 var vg_8 = "suburbs_map_v2.vg.json";
-vegaEmbed("#map8", vg_8, {actions: false}).then(function(result) {
+vegaEmbed("#job_per_sqkm", vg_8, {actions: false}).then(function(result) {
 }).catch(console.error);
 
 var vg_9 = "sankey_v2.vg.json";
-vegaEmbed("#map9", vg_9, {actions: false}).then(function(result) {
+vegaEmbed("#sankey", vg_9, {actions: false}).then(function(result) {
 }).catch(console.error);
 
 var vg_12 = "gender_pay_gap_test.vg.json";
-vegaEmbed("#map12", vg_12, {actions: false}).then(function(result) {
+vegaEmbed("#gender_pay_gap_2", vg_12, {actions: false}).then(function(result) {
 }).catch(console.error);
 
